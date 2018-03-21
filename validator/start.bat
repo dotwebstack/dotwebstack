691688b50f8b6199b71cdcc85c6f194c5bb8d10d
architolk <msj@xs4all.nl>
2018-03-21 21:35:30 +0100

    added validation test as data file

--- validator/start.bat
+++ validator/start.bat
@@ -1,4 +1,4 @@
 @echo off
 cd ..
-java -cp "validator/target/*;validator/target/lib/*" org.dotwebstack.validator.Validator "vocabulary/validation-test.ttl" "vocabulary/elmo/*.ttl" vocabulary/elmo-shacl.ttl
+java -cp "validator/target/*;validator/target/lib/*" org.dotwebstack.validator.Validator "validator/validation-test.ttl" "vocabulary/elmo/*.ttl" vocabulary/elmo-shacl.ttl
 pause
