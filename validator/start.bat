c0b0cf4a8438a2cdc28f9757a15f88bfb7258120
architolk <msj@xs4all.nl>
2018-03-27 14:10:23 +0200

    Changed packaging

--- validator/start.bat
+++ validator/start.bat
@@ -1,4 +1,4 @@
 @echo off
 cd ..
-java -cp "validator/target/*;validator/target/lib/*" org.dotwebstack.validator.Validator "vocabulary/examples/*.trig" "vocabulary/elmo/*.ttl" vocabulary/elmo-shacl.ttl
+java -jar validator/target/validator-0.0.18-SNAPSHOT-jar-with-dependencies.jar "vocabulary/examples/*.trig" "vocabulary/elmo/*.ttl" vocabulary/elmo-shacl.ttl
 pause
