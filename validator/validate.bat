0b24acbf7d7bda4fe933ee76310aed45a38daaab
architolk <msj@xs4all.nl>
2018-04-10 08:08:25 +0200

    Changed version number to 0.0.1-SNAPSHOT

--- validator/validate.bat
+++ validator/validate.bat
@@ -1,4 +1,4 @@
 @echo off
 cd ..
-java -jar validator/target/validator-0.0.18-SNAPSHOT-jar-with-dependencies.jar "vocabulary/examples/*.trig" "vocabulary/elmo/*.ttl" vocabulary/elmo-shacl.ttl validator/report.ttl
+java -jar validator/target/validator-0.0.1-SNAPSHOT-jar-with-dependencies.jar "vocabulary/examples/*.trig" "vocabulary/elmo/*.ttl" vocabulary/elmo-shacl.ttl validator/report.ttl
 pause
