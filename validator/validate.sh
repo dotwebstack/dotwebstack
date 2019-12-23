7383304057e98963217e278a5090af66a5c5d911
architolk <msj@xs4all.nl>
2019-12-23 12:31:03 +0100

    Removed validator

--- validator/validate.sh
+++
@@ -1,3 +0,0 @@
-cd ..
-java -jar validator/target/validator-0.0.1-SNAPSHOT-jar-with-dependencies.jar "vocabulary/examples/*.trig" "vocabulary/elmo/*.ttl" vocabulary/elmo-shacl.ttl validator/report.ttl
-cd validator
