a15849d794fd92dd503f205d1aa53bebaeecc7d3
architolk <msj@xs4all.nl>
2019-12-23 12:05:50 +0100

    Removed validator

--- validator/validate.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd ..
-java -jar validator/target/validator-0.0.1-SNAPSHOT-jar-with-dependencies.jar "vocabulary/examples/*.trig" "vocabulary/elmo/*.ttl" vocabulary/elmo-shacl.ttl validator/report.ttl
-pause
